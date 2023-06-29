4997fb44f99fa4a8c12b14db49655211252b2e69
Dev Sec <dev.beats57@gmail.com>
2023-06-29 05:48:21 +0300

    up

--- assets/js/mainblog.js
+++ assets/js/mainblog.js
@@ -3,7 +3,13 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+// ========================================================================= //
+  //  scroll smooth
+  // ========================================================================= //
 
+  const page = document.getElementById(".page1")
+
+  page.scrollIntoView({behavior: "smooth"})
 (function($) {
 
 	var	$window = $(window),
