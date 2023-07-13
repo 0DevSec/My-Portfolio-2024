da838086f3c51a98e97ffaac075107954caf2af4
Dev Sec <dev.beats57@gmail.com>
2023-07-14 01:51:31 +0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,9 +169,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 1,
+      backSpeed: 7,
+      backDelay: 3000
     });
   }
 
